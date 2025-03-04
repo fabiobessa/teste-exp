f053f58e22b3f4eeab22ba7d1b3ca0a7a70bf2d9
fabiobessa <fabiobessa97@gmail.com>
2025-03-04 12:36:11 -0300

    [WIP] Finaliza estrutura do componente e começo de estilização

--- src/components/AccordionComponent.vue
+++ src/components/AccordionComponent.vue
@@ -1,20 +1,39 @@
 <template>
-  <div class="accordion" id="accordionPanelsStayOpenExample">
+  <div class="accordion" :id="`accordionPanelsStayOpen-${sanitizedTitle}`">
     <div class="accordion-item">
-      <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-        <button class="accordion-button" type="button" data-bs-toggle="collapse"
-          data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-          Accordion Item #1
+      <h2 class="accordion-header" :id="`panelsStayOpen-heading-${sanitizedTitle}`">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+          :data-bs-target="`#panelsStayOpen-${sanitizedTitle}`" aria-expanded="false"
+          :aria-controls="`panelsStayOpen-${sanitizedTitle}`">
+          <div>
+            <span>
+              {{ title }}
+            </span>
+            <div class="d-flex gap-3">
+              <p>
+                Aulas <strong>{{ lessons.quantity }} | {{ lessons.duration }}</strong>
+              </p>
+              <p>
+                Exercícios <strong>{{ exercises.quantity }} | {{ exercises.duration }}</strong>
+              </p>
+              <p>
+                Materiais <strong>{{ materials.quantity }} | {{ materials.duration }}</strong>
+              </p>
+            </div>
+          </div>
         </button>
+
       </h2>
-      <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show"
-        aria-labelledby="panelsStayOpen-headingOne">
-        <div class="accordion-body">
-          <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin
-          adds the appropriate classes that we use to style each element. These classes control the overall appearance,
-          as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or
-          overriding our default variables. It's also worth noting that just about any HTML can go within the
-          <code>.accordion-body</code>, though the transition does limit overflow.
+      <div :id="`panelsStayOpen-${sanitizedTitle}`" class="accordion-collapse collapse"
+        :aria-labelledby="`panelsStayOpen-heading-${sanitizedTitle}`"
+        :data-bs-parent="`#accordionPanelsStayOpen-${sanitizedTitle}`">
+        <div v-if="typeof content === 'string'" class="accordion-body">
+          {{ content }}
+        </div>
+        <div v-else class="accordion-body">
+          <AccordionComponent v-for="subTopic in content" :key="subTopic.name" :title="subTopic.name"
+            :content="subTopic.contentId ? subTopic.contentId : subTopic.subTopics" :lessons="subTopic.lessons"
+            :exercises="subTopic.exercises" :materials="subTopic.materials" />
         </div>
       </div>
     </div>
@@ -22,6 +41,32 @@
 </template>
 
 <script setup lang="ts">
+import type { Exercise, Lesson, Material, Topic } from '@/interface/caderno.interface';
+import { computed } from 'vue';
 
+
+const props = defineProps<{
+  title: string
+  content?: string | Topic[]
+  lessons: Lesson
+  exercises: Exercise
+  materials: Material
+}>()
+
+const sanitizedTitle = computed(() => {
+  return props.title.replace(/[^a-zA-Z0-9-_]/g, '')
+})
 
 </script>
+
+<style scoped>
+.accordion-button:not(.collapsed) {
+  background-color: rgba(236, 202, 252, 255);
+  color: rgba(45, 52, 68, 255);
+}
+
+.accordion-button:focus {
+  border: none;
+  box-shadow: none;
+}
+</style>
---
+++ src/components/BreadCrumb.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li v-for="(item, index) in history" :key="`${item.name}-${index}}`" class="breadcrumb-item"
+        :class="{ 'active': index === history.length - 1 }">
+        <a v-if="index != history.length - 1" :href="item.path">{{item.name}}</a>
+        <span v-else>{{ item.name }}</span>
+      </li>
+    </ol>
+  </nav>
+</template>
+
+<script setup lang="ts">
+interface HistoryItem {
+  name: string
+  path: string
+}
+
+defineProps < { history: HistoryItem[] } > ()
+</script>
